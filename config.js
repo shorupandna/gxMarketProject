c1585ff1c94c32bd0d70fb9d1ce6445a310062a8
Karthik Puranam <karthik.p@nvestbank.com>
2020-02-06 05:57:02 +0000

    added new mongodb atlas connection string

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = function () {
             return {
                 //MONGO_LOCAL_PATH: 'mongodb://localhost:27017/gxmarkets_mainnet',   //---local
                 // MONGO_LOCAL_PATH: "mongodb://gxmm:gxmm270719@13.59.230.221:27017/gxmarkets",
-                MONGO_SERVER_PATH: "mongodb://gxmm123:gxmm270819@13.59.230.221:27017/gxmarkets_mainnet", //--server  
+                MONGO_SERVER_PATH: "mongodb+srv://nvest:bYvoD0kjRtUfYbkS@nvestdb-zzqa4.mongodb.net/test?retryWrites=true&w=majority", //--server  
                     APP_PORT: 3486,
                     mainnet_market: "0xffd7fbb7b8384d8c05647f4b094409135843cd0b"
             };
