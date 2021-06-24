421cfee21bd510f51ae962cb404b864e15882965
karthik.p <karthik.p@nvestbank.com>
2021-06-24 14:20:41 +0530

    changed db url

--- app.js
+++ app.js
@@ -3,88 +3,106 @@
  * author : Saileela Puvvada
  */
 
-const express = require('express');
+const express = require("express");
 let app = express();
-let bodyParser = require('body-parser');
-let methodOverride = require('method-override');
-let configAuth = require('./config')();
-let routes = require('./routes');
-const mongoose = require('mongoose');
-app.use(bodyParser.json({
-    limit: '10mb'
-})); // pull information from html in POST
-app.use(bodyParser.urlencoded({
-    limit: '10mb',
-    extended: true
-}));
+let bodyParser = require("body-parser");
+let methodOverride = require("method-override");
+let configAuth = require("./config")();
+let routes = require("./routes");
+const mongoose = require("mongoose");
+app.use(
+  bodyParser.json({
+    limit: "10mb",
+  })
+); // pull information from html in POST
+app.use(
+  bodyParser.urlencoded({
+    limit: "10mb",
+    extended: true,
+  })
+);
 app.use(methodOverride());
 
 /*Allow CORS*/
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, Accept');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization, Accept"
+  );
+  next();
 });
 
 // MongoDB configuration
 // Database connect options
 var options = {
-    poolSize: 2,
-    keepAlive: 300000,
-    connectTimeoutMS: 30000,
-    autoReconnect: true,
-    reconnectTries: 300000,
-    reconnectInterval: 5000,
-    useNewUrlParser: true,
-    auto_reconnect: true,
-    dbName: 'gxmarkets'
+  poolSize: 2,
+  keepAlive: 300000,
+  connectTimeoutMS: 30000,
+  autoReconnect: true,
+  reconnectTries: 300000,
+  reconnectInterval: 5000,
+  useNewUrlParser: true,
+  auto_reconnect: true,
+  dbName: "gxmarketsnew1029",
 };
 
 var connectWithRetry = function () {
-    return mongoose.connect(configAuth.MONGO_SERVER_PATH, options, function (err) {
-        if (err) {
-            console.log('Failed to connect to mongo on startup - retrying in 5 sec', err);
-            setTimeout(connectWithRetry, 5000);
-        }
-    });
+  return mongoose.connect(
+    configAuth.MONGO_SERVER_PATH,
+    options,
+    function (err) {
+      if (err) {
+        console.log(
+          "Failed to connect to mongo on startup - retrying in 5 sec",
+          err
+        );
+        setTimeout(connectWithRetry, 5000);
+      }
+    }
+  );
 };
 connectWithRetry();
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + configAuth.MONGO_SERVER_PATH);
+mongoose.connection.on("connected", function () {
+  console.log(
+    "Mongoose default connection open to " + configAuth.MONGO_SERVER_PATH
+  );
 });
 // If the connection throws an error
-mongoose.connection.on('error', function (err) {
-    console.log(err, 'Mongoose default connection error');
+mongoose.connection.on("error", function (err) {
+  console.log(err, "Mongoose default connection error");
 });
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
-    //  connectWithRetry();
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose default connection disconnected");
+  //  connectWithRetry();
 });
 // When the connection is reconnected
-mongoose.connection.on('reconnected', function () {
-    console.log('Mongoose default connection reconnected');
+mongoose.connection.on("reconnected", function () {
+  console.log("Mongoose default connection reconnected");
 });
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  });
 });
 console.log(" /");
 
-app.use('/gxmm', routes);
+app.use("/gxmm", routes);
 
-app.get('/', function (req, res) {
-    res.send('Gx Money Markets');
-    console.log('Gx Money Markets');
+app.get("/", function (req, res) {
+  res.send("Gx Money Markets");
+  console.log("Gx Money Markets");
 });
 
 app.listen(configAuth.APP_PORT);
 
-console.log('Port running on ' + configAuth.APP_PORT);
+console.log("Port running on " + configAuth.APP_PORT);
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/* 
+/*
  *  Configuration
  * @module      :: Model
  * @description :: Represent data for the Default Configuration
@@ -7,16 +7,17 @@
  */
 
 module.exports = function () {
-    switch (process.env.NODE_ENV) {
-        default:
-        case 'dev':
-            return {
-                //MONGO_LOCAL_PATH: 'mongodb://localhost:27017/gxmarkets_mainnet',   //---local
-                // MONGO_LOCAL_PATH: "mongodb://gxmm:gxmm270719@13.59.230.221:27017/gxmarkets",
-                MONGO_SERVER_PATH: "mongodb+srv://nvest:bYvoD0kjRtUfYbkS@nvestdb-zzqa4.mongodb.net/test?retryWrites=true&w=majority", //--server  
-                    APP_PORT: 3486,
-                    mainnet_market: "0xffd7fbb7b8384d8c05647f4b094409135843cd0b"
-            };
-            break;
-    }
+  switch (process.env.NODE_ENV) {
+    default:
+    case "dev":
+      return {
+        //MONGO_LOCAL_PATH: 'mongodb://localhost:27017/gxmarkets_mainnet',   //---local
+        // MONGO_LOCAL_PATH: "mongodb://gxmm:gxmm270719@13.59.230.221:27017/gxmarkets",
+        MONGO_SERVER_PATH:
+          "mongodb+srv://karthik:9M0S95Ac0ub2Qhdx@cluster0.ecdne.mongodb.net/nvestbrain?retryWrites=true&w=majority", //--server
+        APP_PORT: 3486,
+        mainnet_market: "0xffd7fbb7b8384d8c05647f4b094409135843cd0b",
+      };
+      break;
+  }
 };
