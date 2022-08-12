8a72019864f30e042542b12cdf8ba13847196424
rroopkumar <rpkmrroopkumar8@gmail.com>
2022-08-12 15:53:03 +0530

    adding the testnet endpoint

--- web3init.js
+++ web3init.js
@@ -4,7 +4,8 @@ let endpoint;
 if (process.env.NODE_ENV === 'production') {
     endpoint = 'https://mainnet.infura.io/v3/8b2d98b08efb4b0ca12e3ca7653d54b1';
 } else {
-    endpoint = 'https://mainnet.infura.io/v3/8b2d98b08efb4b0ca12e3ca7653d54b1';
+    // endpoint = 'https://mainnet.infura.io/v3/8b2d98b08efb4b0ca12e3ca7653d54b1';
+    endpoint = 'https://ropsten.infura.io/v3/d40fef3535244522b72e286039b8705b';
 }
 
 module.exports = function () {
